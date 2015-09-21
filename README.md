8f5346ec62f49f7757fd7a003ab68945c0c79180
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2015-09-21 14:30:09 +0200

    Better API

--- README.md
+++ README.md
@@ -14,26 +14,26 @@ npm install burst_data_map
 ## Usage
 
 ```javascript
-import {createMap, dataFor} from 'burst_data_map'
+import {createDataMap, getDataFor, setDataFor} from 'burst_data_map'
 
 
 var myObject = {};
 
 
-var myMap = createMap({weak: true})
-dataFor(myMap, myObject) // -> undefined
+var myMap = createDataMap({weak: true})
+getDataFor(myMap, myObject) // -> undefined
 
 
-var myMap = createMap({weak: true, defaultData: => 'foo'})
-dataFor(myMap, myObject) // -> 'foo'
+var myMap = createDataMap({weak: true, defaultData: => 'foo'})
+getDataFor(myMap, myObject) // -> 'foo'
 
 
-var myMap = createMap({weak: true, defaultData: => 'foo'})
-myMap.set(myObject, 'bar') // Native API
-dataFor(myMap, myObject) // -> 'bar'
+var myMap = createDataMap({weak: true, defaultData: => 'foo'})
+setDataFor(myMap, myObject, 'bar')
+getDataFor(myMap, myObject) // -> 'bar'
 
 
-var myMap = createMap({weak: true})
-myMap.set('hello', 'world')
-dataFor(myMap, 'hello') // -> 'world'
+var myMap = createDataMap({weak: true})
+setDataFor(myMap, 'hello', 'world')
+getDataFor(myMap, 'hello') // -> 'world'
 ```
--- data_map.js
+++ data_map.js
@@ -2,7 +2,8 @@ var defaultDataMap = createMap({weak: true})
 var namespacesMap = createMap({weak: true, => {}})
 
 
-export function createMap (options = {weak: false}) {
+
+export function createDataMap (options = {weak: false}) {
     let map = options.weak ? new WeakMap() : new Map()
     if ('defaultData' in options) {
         defaultDataMap.set(map, options.defaultData())
@@ -11,17 +12,31 @@ export function createMap (options = {weak: false}) {
 }
 
 
-export function dataFor (map, object) {
 
-    if (typeof object === 'string' && map instanceof WeakMap) {
-        let namespaces = dataFor(namespacesMap, map)
-        namespaces[object] = namespaces[object] || Symbol()
-        object = namespaces[object]
-    }
+export function getDataFor (map, object) {
+    object = formatIdentifierObject(map, object)
 
     if (!map.has(object)) {
         map.set(object, defaultDataMap.get(map))
     }
 
     return map.get(object)
+}
+
+
+
+export function setDataFor (map, object, data) {
+    map.set(formatIdentifierObject(map, object), data)
+}
+
+
+
+function formatIdentifierObject (map, object) {
+    if (typeof object === 'string' && map instanceof WeakMap) {
+        let namespaces = dataFor(namespacesMap, map)
+        namespaces[object] = namespaces[object] || Symbol()
+        object = namespaces[object]
+    }
+
+    return object
 }
